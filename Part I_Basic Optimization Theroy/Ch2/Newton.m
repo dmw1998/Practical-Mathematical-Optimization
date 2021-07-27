8cebdaad23b9bba4932e30a6a0c922a88b1e4b2d
dmw1998 <dmw1998@users.noreply.github.com>
2021-07-28 02:53:54 +0800

    Create Newton.m

---
+++ Part I_Basic Optimization Theroy/Ch2/Newton.m
@@ -0,0 +1,26 @@
+function [x, minf, iter] = Newton(x0, f, df, H, epsilon)
+% This function compute the minimum minf of function f with the tolerance
+% epsilon and output the minimum point x, the minimum minf and iterations
+% used.
+
+% x0: initial point
+% f: function
+% df: the gradiant of function f
+% H: the Hessian matrix of function f
+% epsilon: tolerance
+
+f0 = f(x0);
+f1 = 10^6;
+iter = 0;
+while abs(f0 - f1) > epsilon
+    iter = iter + 1;
+    
+    f0 = f1;
+    
+    x1 = x0 - H(x0)^(-1) * df(x0);
+    
+    f1 = f(x1);
+    
+end
+
+end
